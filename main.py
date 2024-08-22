1afb83655f1d8748ef8d44bd110d2ebfae802176
unknown <chirag7bravo@gmail.com>
2024-08-22 16:01:31 +0530

    Making a change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+## A Code by Chirag...
 a = int(input("Enter a Number: "))
 b = int(input("Enter a Number: "))
 sum = a + b
 print(f"Sum of {a} and {b} is: {sum}")
+print("Making a chnage in the Code")
